3b36fe73bfa88e36e488e29ef5f029d0291c4aa9
dhanoahs3 <dhanoahs3@gmail.com>
2016-11-13 11:40:04 -0800

    first commit

---
+++ src/test/java/package1/AppTest.java
@@ -0,0 +1,16 @@
+package package1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+
+
+public class AppTest 
+{  @Test
+	public static void google_test(){
+		System.out.println("In the test-----------------------------------App Test");
+	}
+	
+}
---
+++ src/test/java/package1/Yahoo_test.java
@@ -0,0 +1,12 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class Yahoo_test {
+
+	
+	  @Test
+		public static void google_test(){
+			System.out.println("In the test---------------------------Yahoo Test");
+		}
+}
